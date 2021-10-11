98ed0ff198cc1b136413dcd50dbd3217ec14e019
Alex-the-666 <alex.rowlands@cox.net>
2021-10-11 11:47:16 -0700

    fixed missing roadrunner boots model

--- src/main/java/com/github/alexthe666/alexsmobs/client/model/layered/ModelRoadrunnerBoots.java
+++ src/main/java/com/github/alexthe666/alexsmobs/client/model/layered/ModelRoadrunnerBoots.java
@@ -17,14 +17,14 @@ public class ModelRoadrunnerBoots extends HumanoidModel {
     }
 
     public static LayerDefinition createArmorLayer(CubeDeformation deformation) {
-        MeshDefinition meshdefinition = HumanoidModel.createMesh(deformation, 0.0F);
+        MeshDefinition meshdefinition = HumanoidModel.createMesh(deformation, 1.0F);
         PartDefinition partdefinition = meshdefinition.getRoot();
         PartDefinition leftleg = partdefinition.getChild("left_leg");
         PartDefinition rightleg = partdefinition.getChild("right_leg");
 
-        leftleg.addOrReplaceChild("featherl", CubeListBuilder.create().texOffs(20, 22).addBox(-3.0F, -7.5F, 0.0F, 3.0F, 8.0F, 0.0F, deformation), PartPose.offsetAndRotation(-2.5F, 9.5F, 0.4F, 0.0F, 0.9773843811168246F, -0.3127630032889644F));
-        rightleg.addOrReplaceChild("featherr", CubeListBuilder.create().texOffs(20, 22).mirror().addBox(0.0F, -7.4F, 0.0F, 3.0F, 8.0F, 0.0F, deformation), PartPose.offsetAndRotation(2.5F, 9.5F, -0.4F, 0.0F, -0.9773843811168246F, 0.3127630032889644F));
+        rightleg.addOrReplaceChild("featherr", CubeListBuilder.create().texOffs(20, 22).addBox(-3.0F, -7.5F, 0.0F, 3.0F, 8.0F, 0.0F, deformation), PartPose.offsetAndRotation(-2F, 9.5F, 0.4F, 0.0F, 0.9773843811168246F, -0.3127630032889644F));
+        leftleg.addOrReplaceChild("featherl", CubeListBuilder.create().texOffs(20, 22).mirror().addBox(0.0F, -7.4F, 0.0F, 3.0F, 8.0F, 0.0F, deformation), PartPose.offsetAndRotation(2F, 9.5F, -0.4F, 0.0F, -0.9773843811168246F, 0.3127630032889644F));
 
-        return LayerDefinition.create(meshdefinition, 64, 64);
+        return LayerDefinition.create(meshdefinition, 64, 32);
     }
 }
