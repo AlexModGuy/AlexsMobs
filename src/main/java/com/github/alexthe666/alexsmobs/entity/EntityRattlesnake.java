abc7362b52db34e7557d9c00b9025b04c4443a78
gohkenytp <34868610+gohkenytp@users.noreply.github.com>
2021-10-24 14:59:06 -0400

    Rattlesnakes can now follow players when they have their rattles
    Based off from Logdotzip's version of his Rattlesnake entity

--- src/main/java/com/github/alexthe666/alexsmobs/entity/EntityRattlesnake.java
+++ src/main/java/com/github/alexthe666/alexsmobs/entity/EntityRattlesnake.java
@@ -64,6 +64,7 @@ public class EntityRattlesnake extends AnimalEntity implements IAnimatedEntity {
         this.goalSelector.addGoal(2, new WarnPredatorsGoal());
         this.goalSelector.addGoal(2, new BreedGoal(this, 1.0D));
         this.goalSelector.addGoal(4, new FollowParentGoal(this, 1.1D));
+        this.goalSelector.addGoal(4, new TemptGoal(this, 1.1D, Ingredient.fromTag(ItemTags.getCollection().get(AMTagRegistry.RATTLESNAKE_RATTLE)), false));
         this.goalSelector.addGoal(5, new AnimalAIWanderRanged(this, 60, 1.0D, 7, 7));
         this.goalSelector.addGoal(6, new LookAtGoal(this, PlayerEntity.class, 15.0F));
         this.goalSelector.addGoal(7, new LookRandomlyGoal(this));
