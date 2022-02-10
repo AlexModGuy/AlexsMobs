a8b32ed300fb98236f5c008898b9deaefc8032e6
Alex-the-666 <alex.rowlands@cox.net>
2022-02-10 13:05:20 -0800

    made baby rattlesnakes more dangerous than adults

--- src/main/java/com/github/alexthe666/alexsmobs/entity/EntityRattlesnake.java
+++ src/main/java/com/github/alexthe666/alexsmobs/entity/EntityRattlesnake.java
@@ -170,9 +170,10 @@ public class EntityRattlesnake extends Animal implements IAnimatedEntity {
         LivingEntity target = this.getTarget();
         if(this.getAnimation() == ANIMATION_BITE && this.getAnimationTick() == 8 && target != null && this.distanceTo(target) < 2D){
             boolean meepMeep = target instanceof EntityRoadrunner;
-            target.hurt(DamageSource.mobAttack(this), meepMeep ? 1.0F : (float)getAttribute(Attributes.ATTACK_DAMAGE).getBaseValue());
+            int f = isBaby() ? 2 : 1;
+            target.hurt(DamageSource.mobAttack(this), meepMeep ? 1.0F : f * (float)getAttribute(Attributes.ATTACK_DAMAGE).getBaseValue());
             if(!meepMeep){
-                target.addEffect(new MobEffectInstance(MobEffects.POISON, 300, 2));
+                target.addEffect(new MobEffectInstance(MobEffects.POISON, 300, f * 2));
             }
         }
         if(isRattling()){
