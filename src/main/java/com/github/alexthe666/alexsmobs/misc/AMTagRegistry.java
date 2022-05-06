061279f37db07ac9526751d5e32849736a4eaec6
Alex-the-666 <alex.rowlands@cox.net>
2022-05-06 09:58:02 -0700

    raccoon tweaks

--- src/main/java/com/github/alexthe666/alexsmobs/misc/AMTagRegistry.java
+++ src/main/java/com/github/alexthe666/alexsmobs/misc/AMTagRegistry.java
@@ -67,7 +67,7 @@ public class AMTagRegistry {
     public static final TagKey<Block> GELADA_MONKEY_GRASS = registerBlockTag("gelada_monkey_grass");
     public static final TagKey<EntityType<?>> GIANT_SQUID_TARGETS = registerEntityTag("giant_squid_targets");
     public static final TagKey<EntityType<?>> MONKEY_TARGET_WITH_DART = registerEntityTag("monkey_target_with_dart");
-
+    public static final TagKey<Item> RACOON_DISSOLVES = registerItemTag("raccoon_dissolves");
     public static final TagKey<Block> ROADRUNNER_SPAWNS = registerBlockTag("roadrunner_spawns");
     public static final TagKey<Block> LOBSTER_SPAWNS = registerBlockTag("lobster_spawns");
     public static final TagKey<Block> MIMIC_OCTOPUS_SPAWNS = registerBlockTag("mimic_octopus_spawns");
