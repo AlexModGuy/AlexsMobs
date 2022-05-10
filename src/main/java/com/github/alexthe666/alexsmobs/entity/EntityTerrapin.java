5156954b6fd5be35504c7fb2e0f1e52da7da81e9
Alex-the-666 <alex.rowlands@cox.net>
2022-05-09 22:33:00 -0700

    fix #1065 fix #1071 fix #1063

--- src/main/java/com/github/alexthe666/alexsmobs/entity/EntityTerrapin.java
+++ src/main/java/com/github/alexthe666/alexsmobs/entity/EntityTerrapin.java
@@ -611,6 +611,8 @@ public class EntityTerrapin extends Animal implements ISemiAquatic, Bucketable {
             this.turtle.setHasEgg(true);
             this.animal.resetLove();
             this.partner.resetLove();
+            this.animal.setAge(6000);
+            this.partner.setAge(6000);
             Random random = this.animal.getRandom();
             if (this.level.getGameRules().getBoolean(GameRules.RULE_DOMOBLOOT)) {
                 this.level.addFreshEntity(new ExperienceOrb(this.level, this.animal.getX(), this.animal.getY(), this.animal.getZ(), random.nextInt(7) + 1));
