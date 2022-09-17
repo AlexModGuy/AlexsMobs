18713e578038b4baa635b5aa45111c3dbbf79a91
AlexModGuy <alex.rowlands@cox.net>
2022-09-17 10:35:39 -0700

    fix server crash with __ClientEvents_onRenderWorldLastEvent_RenderLevelLastEvent

--- src/main/java/com/github/alexthe666/alexsmobs/client/render/AMItemstackRenderer.java
+++ src/main/java/com/github/alexthe666/alexsmobs/client/render/AMItemstackRenderer.java
@@ -38,92 +38,6 @@ import java.util.Map;
 
 public class AMItemstackRenderer extends BlockEntityWithoutLevelRenderer {
 
-    private static List<Pair<EntityType, Float>> MOB_ICONS = Util.make(Lists.newArrayList(), (list) -> {
-        list.add(new Pair<>(AMEntityRegistry.GRIZZLY_BEAR.get(),  0.6F));
-        list.add(new Pair<>(AMEntityRegistry.ROADRUNNER.get(),  0.8F));
-        list.add(new Pair<>(AMEntityRegistry.BONE_SERPENT.get(),  0.55F));
-        list.add(new Pair<>(AMEntityRegistry.GAZELLE.get(),  0.6F));
-        list.add(new Pair<>(AMEntityRegistry.CROCODILE.get(),  0.3F));
-        list.add(new Pair<>(AMEntityRegistry.FLY.get(),  1.3F));
-        list.add(new Pair<>(AMEntityRegistry.HUMMINGBIRD.get(),  1.5F));
-        list.add(new Pair<>(AMEntityRegistry.ORCA.get(),  0.2F));
-        list.add(new Pair<>(AMEntityRegistry.SUNBIRD.get(),  0.2F));
-        list.add(new Pair<>(AMEntityRegistry.GORILLA.get(),  0.85F));
-        list.add(new Pair<>(AMEntityRegistry.CRIMSON_MOSQUITO.get(),  0.6F));
-        list.add(new Pair<>(AMEntityRegistry.RATTLESNAKE.get(),  0.6F));
-        list.add(new Pair<>(AMEntityRegistry.ENDERGRADE.get(),  0.8F));
-        list.add(new Pair<>(AMEntityRegistry.HAMMERHEAD_SHARK.get(),  0.5F));
-        list.add(new Pair<>(AMEntityRegistry.LOBSTER.get(),  0.85F));
-        list.add(new Pair<>(AMEntityRegistry.KOMODO_DRAGON.get(),  0.5F));
-        list.add(new Pair<>(AMEntityRegistry.CAPUCHIN_MONKEY.get(),  0.85F));
-        list.add(new Pair<>(AMEntityRegistry.CENTIPEDE_HEAD.get(),  0.65F));
-        list.add(new Pair<>(AMEntityRegistry.WARPED_TOAD.get(),  0.6F));
-        list.add(new Pair<>(AMEntityRegistry.MOOSE.get(),  0.38F));
-        list.add(new Pair<>(AMEntityRegistry.MIMICUBE.get(),  0.95F));
-        list.add(new Pair<>(AMEntityRegistry.RACCOON.get(),  0.8F));
-        list.add(new Pair<>(AMEntityRegistry.BLOBFISH.get(),  1F));
-        list.add(new Pair<>(AMEntityRegistry.SEAL.get(),  0.5F));
-        list.add(new Pair<>(AMEntityRegistry.COCKROACH.get(),  1F));
-        list.add(new Pair<>(AMEntityRegistry.SHOEBILL.get(),  0.8F));
-        list.add(new Pair<>(AMEntityRegistry.ELEPHANT.get(),  0.3F));
-        list.add(new Pair<>(AMEntityRegistry.SOUL_VULTURE.get(),  0.8F));
-        list.add(new Pair<>(AMEntityRegistry.SNOW_LEOPARD.get(),  0.7F));
-        list.add(new Pair<>(AMEntityRegistry.SPECTRE.get(),  0.3F));
-        list.add(new Pair<>(AMEntityRegistry.CROW.get(),  1.3F));
-        list.add(new Pair<>(AMEntityRegistry.ALLIGATOR_SNAPPING_TURTLE.get(),  0.65F));
-        list.add(new Pair<>(AMEntityRegistry.MUNGUS.get(),  0.7F));
-        list.add(new Pair<>(AMEntityRegistry.MANTIS_SHRIMP.get(),  0.7F));
-        list.add(new Pair<>(AMEntityRegistry.GUSTER.get(),  0.55F));
-        list.add(new Pair<>(AMEntityRegistry.WARPED_MOSCO.get(),  0.35F));
-        list.add(new Pair<>(AMEntityRegistry.STRADDLER.get(),  0.38F));
-        list.add(new Pair<>(AMEntityRegistry.STRADPOLE.get(),  0.9F));
-        list.add(new Pair<>(AMEntityRegistry.EMU.get(),  0.7F));
-        list.add(new Pair<>(AMEntityRegistry.PLATYPUS.get(),  1F));
-        list.add(new Pair<>(AMEntityRegistry.DROPBEAR.get(),  0.65F));
-        list.add(new Pair<>(AMEntityRegistry.TASMANIAN_DEVIL.get(),  1.2F));
-        list.add(new Pair<>(AMEntityRegistry.KANGAROO.get(),  0.7F));
-        list.add(new Pair<>(AMEntityRegistry.CACHALOT_WHALE.get(),  0.1F));
-        list.add(new Pair<>(AMEntityRegistry.LEAFCUTTER_ANT.get(),  1.2F));
-        list.add(new Pair<>(AMEntityRegistry.ENDERIOPHAGE.get(),  0.65F));
-        list.add(new Pair<>(AMEntityRegistry.BALD_EAGLE.get(),  0.85F));
-        list.add(new Pair<>(AMEntityRegistry.TIGER.get(),  0.65F));
-        list.add(new Pair<>(AMEntityRegistry.TARANTULA_HAWK.get(),  0.7F));
-        list.add(new Pair<>(AMEntityRegistry.VOID_WORM.get(),  0.3F));
-        list.add(new Pair<>(AMEntityRegistry.FRILLED_SHARK.get(),  0.65F));
-        list.add(new Pair<>(AMEntityRegistry.MIMIC_OCTOPUS.get(),  0.7F));
-        list.add(new Pair<>(AMEntityRegistry.SEAGULL.get(),  1.2F));
-        list.add(new Pair<>(AMEntityRegistry.FROSTSTALKER.get(),  0.8F));
-        list.add(new Pair<>(AMEntityRegistry.TUSKLIN.get(),  0.6F));
-        list.add(new Pair<>(AMEntityRegistry.LAVIATHAN.get(),  0.2F));
-        list.add(new Pair<>(AMEntityRegistry.COSMAW.get(),  0.32F));
-        list.add(new Pair<>(AMEntityRegistry.TOUCAN.get(),  1.3F));
-        list.add(new Pair<>(AMEntityRegistry.MANED_WOLF.get(),  0.85F));
-        list.add(new Pair<>(AMEntityRegistry.ANACONDA.get(),  1.0F));
-        list.add(new Pair<>(AMEntityRegistry.ANTEATER.get(),  0.5F));
-        list.add(new Pair<>(AMEntityRegistry.ROCKY_ROLLER.get(),  0.65F));
-        list.add(new Pair<>(AMEntityRegistry.FLUTTER.get(),  1.15F));
-        list.add(new Pair<>(AMEntityRegistry.GELADA_MONKEY.get(),  0.65F));
-        list.add(new Pair<>(AMEntityRegistry.JERBOA.get(),  1.3F));
-        list.add(new Pair<>(AMEntityRegistry.TERRAPIN.get(),  1.1F));
-        list.add(new Pair<>(AMEntityRegistry.COMB_JELLY.get(),  1.0F));
-        list.add(new Pair<>(AMEntityRegistry.COSMIC_COD.get(),  1.3F));
-        list.add(new Pair<>(AMEntityRegistry.BUNFUNGUS.get(),  0.5F));
-        list.add(new Pair<>(AMEntityRegistry.BISON.get(),  0.45F));
-        list.add(new Pair<>(AMEntityRegistry.GIANT_SQUID.get(),  0.3F));
-        list.add(new Pair<>(AMEntityRegistry.DEVILS_HOLE_PUPFISH.get(),  1.4F));
-        list.add(new Pair<>(AMEntityRegistry.CATFISH.get(),  1.15F));
-        list.add(new Pair<>(AMEntityRegistry.FLYING_FISH.get(),  1.2F));
-        list.add(new Pair<>(AMEntityRegistry.SKELEWAG.get(),  0.5F));
-        list.add(new Pair<>(AMEntityRegistry.RAIN_FROG.get(),  1.8F));
-        list.add(new Pair<>(AMEntityRegistry.POTOO.get(),  1.2F));
-        list.add(new Pair<>(AMEntityRegistry.MUDSKIPPER.get(),  1.2F));
-        list.add(new Pair<>(AMEntityRegistry.RHINOCEROS.get(),  0.4F));
-        list.add(new Pair<>(AMEntityRegistry.SUGAR_GLIDER.get(),  1.3F));
-        list.add(new Pair<>(AMEntityRegistry.FARSEER.get(),  0.6F));
-        list.add(new Pair<>(AMEntityRegistry.SKREECHER.get(),  0.77F));
-        list.add(new Pair<>(AMEntityRegistry.UNDERMINER.get(),  0.65F));
-        list.add(new Pair<>(AMEntityRegistry.MURMUR.get(),  0.65F));
-    });
     public static int ticksExisted = 0;
     private static final ModelShieldOfTheDeep SHIELD_OF_THE_DEEP_MODEL = new ModelShieldOfTheDeep();
     private static final ResourceLocation SHIELD_OF_THE_DEEP_TEXTURE = new ResourceLocation("alexsmobs:textures/armor/shield_of_the_deep.png");
@@ -153,8 +67,8 @@ public class AMItemstackRenderer extends BlockEntityWithoutLevelRenderer {
         ticksExisted++;
     }
 
-    private static float getScaleFor(EntityType type) {
-        for (Pair<EntityType, Float> pair : MOB_ICONS) {
+    private static float getScaleFor(EntityType type, List<Pair<EntityType, Float>> mobIcons) {
+        for (Pair<EntityType, Float> pair : mobIcons) {
             if (pair.getFirst() == type) {
                 return pair.getSecond();
             }
@@ -348,14 +262,15 @@ public class AMItemstackRenderer extends BlockEntityWithoutLevelRenderer {
         */
         if (itemStackIn.getItem() == AMItemRegistry.TAB_ICON.get()) {
             Entity fakeEntity = null;
-            int entityIndex = (tick / 40) % (MOB_ICONS.size());
+            List<Pair<EntityType, Float>> mobIcons = AMMobIcons.getMobIcons();
+            int entityIndex = (tick / 40) % (mobIcons.size());
             float scale = 1.0F;
             int flags = 0;
             if (ItemTabIcon.hasCustomEntityDisplay(itemStackIn)) {
                 flags = itemStackIn.getTag().getInt("DisplayMobFlags");
                 String index = ItemTabIcon.getCustomDisplayEntityString(itemStackIn);
                 EntityType local =  ItemTabIcon.getEntityType(itemStackIn.getTag());
-                scale = getScaleFor(local);
+                scale = getScaleFor(local, mobIcons);
                 if(itemStackIn.getTag().getFloat("DisplayMobScale") > 0){
                     scale = itemStackIn.getTag().getFloat("DisplayMobScale");
                 }
@@ -370,8 +285,8 @@ public class AMItemstackRenderer extends BlockEntityWithoutLevelRenderer {
                     fakeEntity = this.renderedEntites.get(local.getDescriptionId());
                 }
             } else {
-                EntityType type = MOB_ICONS.get(entityIndex).getFirst();
-                scale = MOB_ICONS.get(entityIndex).getSecond();
+                EntityType type = mobIcons.get(entityIndex).getFirst();
+                scale = mobIcons.get(entityIndex).getSecond();
                 if (type != null) {
                     if (this.renderedEntites.get(type.getDescriptionId()) == null) {
                         Entity entity = type.create(Minecraft.getInstance().level);
---
+++ src/main/java/com/github/alexthe666/alexsmobs/client/render/AMMobIcons.java
@@ -0,0 +1,105 @@
+package com.github.alexthe666.alexsmobs.client.render;
+
+import com.github.alexthe666.alexsmobs.entity.AMEntityRegistry;
+import com.google.common.collect.Lists;
+import com.mojang.datafixers.util.Pair;
+import net.minecraft.Util;
+import net.minecraft.world.entity.EntityType;
+
+import java.util.List;
+
+public class AMMobIcons {
+
+    private static List<Pair<EntityType, Float>> MOB_ICONS = Util.make(Lists.newArrayList(), (list) -> {
+        list.add(new Pair<>(AMEntityRegistry.GRIZZLY_BEAR.get(), 0.6F));
+        list.add(new Pair<>(AMEntityRegistry.ROADRUNNER.get(), 0.8F));
+        list.add(new Pair<>(AMEntityRegistry.BONE_SERPENT.get(), 0.55F));
+        list.add(new Pair<>(AMEntityRegistry.GAZELLE.get(), 0.6F));
+        list.add(new Pair<>(AMEntityRegistry.CROCODILE.get(), 0.3F));
+        list.add(new Pair<>(AMEntityRegistry.FLY.get(), 1.3F));
+        list.add(new Pair<>(AMEntityRegistry.HUMMINGBIRD.get(), 1.5F));
+        list.add(new Pair<>(AMEntityRegistry.ORCA.get(), 0.2F));
+        list.add(new Pair<>(AMEntityRegistry.SUNBIRD.get(), 0.2F));
+        list.add(new Pair<>(AMEntityRegistry.GORILLA.get(), 0.85F));
+        list.add(new Pair<>(AMEntityRegistry.CRIMSON_MOSQUITO.get(), 0.6F));
+        list.add(new Pair<>(AMEntityRegistry.RATTLESNAKE.get(), 0.6F));
+        list.add(new Pair<>(AMEntityRegistry.ENDERGRADE.get(), 0.8F));
+        list.add(new Pair<>(AMEntityRegistry.HAMMERHEAD_SHARK.get(), 0.5F));
+        list.add(new Pair<>(AMEntityRegistry.LOBSTER.get(), 0.85F));
+        list.add(new Pair<>(AMEntityRegistry.KOMODO_DRAGON.get(), 0.5F));
+        list.add(new Pair<>(AMEntityRegistry.CAPUCHIN_MONKEY.get(), 0.85F));
+        list.add(new Pair<>(AMEntityRegistry.CENTIPEDE_HEAD.get(), 0.65F));
+        list.add(new Pair<>(AMEntityRegistry.WARPED_TOAD.get(), 0.6F));
+        list.add(new Pair<>(AMEntityRegistry.MOOSE.get(), 0.38F));
+        list.add(new Pair<>(AMEntityRegistry.MIMICUBE.get(), 0.95F));
+        list.add(new Pair<>(AMEntityRegistry.RACCOON.get(), 0.8F));
+        list.add(new Pair<>(AMEntityRegistry.BLOBFISH.get(), 1F));
+        list.add(new Pair<>(AMEntityRegistry.SEAL.get(), 0.5F));
+        list.add(new Pair<>(AMEntityRegistry.COCKROACH.get(), 1F));
+        list.add(new Pair<>(AMEntityRegistry.SHOEBILL.get(), 0.8F));
+        list.add(new Pair<>(AMEntityRegistry.ELEPHANT.get(), 0.3F));
+        list.add(new Pair<>(AMEntityRegistry.SOUL_VULTURE.get(), 0.8F));
+        list.add(new Pair<>(AMEntityRegistry.SNOW_LEOPARD.get(), 0.7F));
+        list.add(new Pair<>(AMEntityRegistry.SPECTRE.get(), 0.3F));
+        list.add(new Pair<>(AMEntityRegistry.CROW.get(), 1.3F));
+        list.add(new Pair<>(AMEntityRegistry.ALLIGATOR_SNAPPING_TURTLE.get(), 0.65F));
+        list.add(new Pair<>(AMEntityRegistry.MUNGUS.get(), 0.7F));
+        list.add(new Pair<>(AMEntityRegistry.MANTIS_SHRIMP.get(), 0.7F));
+        list.add(new Pair<>(AMEntityRegistry.GUSTER.get(), 0.55F));
+        list.add(new Pair<>(AMEntityRegistry.WARPED_MOSCO.get(), 0.35F));
+        list.add(new Pair<>(AMEntityRegistry.STRADDLER.get(), 0.38F));
+        list.add(new Pair<>(AMEntityRegistry.STRADPOLE.get(), 0.9F));
+        list.add(new Pair<>(AMEntityRegistry.EMU.get(), 0.7F));
+        list.add(new Pair<>(AMEntityRegistry.PLATYPUS.get(), 1F));
+        list.add(new Pair<>(AMEntityRegistry.DROPBEAR.get(), 0.65F));
+        list.add(new Pair<>(AMEntityRegistry.TASMANIAN_DEVIL.get(), 1.2F));
+        list.add(new Pair<>(AMEntityRegistry.KANGAROO.get(), 0.7F));
+        list.add(new Pair<>(AMEntityRegistry.CACHALOT_WHALE.get(), 0.1F));
+        list.add(new Pair<>(AMEntityRegistry.LEAFCUTTER_ANT.get(), 1.2F));
+        list.add(new Pair<>(AMEntityRegistry.ENDERIOPHAGE.get(), 0.65F));
+        list.add(new Pair<>(AMEntityRegistry.BALD_EAGLE.get(), 0.85F));
+        list.add(new Pair<>(AMEntityRegistry.TIGER.get(), 0.65F));
+        list.add(new Pair<>(AMEntityRegistry.TARANTULA_HAWK.get(), 0.7F));
+        list.add(new Pair<>(AMEntityRegistry.VOID_WORM.get(), 0.3F));
+        list.add(new Pair<>(AMEntityRegistry.FRILLED_SHARK.get(), 0.65F));
+        list.add(new Pair<>(AMEntityRegistry.MIMIC_OCTOPUS.get(), 0.7F));
+        list.add(new Pair<>(AMEntityRegistry.SEAGULL.get(), 1.2F));
+        list.add(new Pair<>(AMEntityRegistry.FROSTSTALKER.get(), 0.8F));
+        list.add(new Pair<>(AMEntityRegistry.TUSKLIN.get(), 0.6F));
+        list.add(new Pair<>(AMEntityRegistry.LAVIATHAN.get(), 0.2F));
+        list.add(new Pair<>(AMEntityRegistry.COSMAW.get(), 0.32F));
+        list.add(new Pair<>(AMEntityRegistry.TOUCAN.get(), 1.3F));
+        list.add(new Pair<>(AMEntityRegistry.MANED_WOLF.get(), 0.85F));
+        list.add(new Pair<>(AMEntityRegistry.ANACONDA.get(), 1.0F));
+        list.add(new Pair<>(AMEntityRegistry.ANTEATER.get(), 0.5F));
+        list.add(new Pair<>(AMEntityRegistry.ROCKY_ROLLER.get(), 0.65F));
+        list.add(new Pair<>(AMEntityRegistry.FLUTTER.get(), 1.15F));
+        list.add(new Pair<>(AMEntityRegistry.GELADA_MONKEY.get(), 0.65F));
+        list.add(new Pair<>(AMEntityRegistry.JERBOA.get(), 1.3F));
+        list.add(new Pair<>(AMEntityRegistry.TERRAPIN.get(), 1.1F));
+        list.add(new Pair<>(AMEntityRegistry.COMB_JELLY.get(), 1.0F));
+        list.add(new Pair<>(AMEntityRegistry.COSMIC_COD.get(), 1.3F));
+        list.add(new Pair<>(AMEntityRegistry.BUNFUNGUS.get(), 0.5F));
+        list.add(new Pair<>(AMEntityRegistry.BISON.get(), 0.45F));
+        list.add(new Pair<>(AMEntityRegistry.GIANT_SQUID.get(), 0.3F));
+        list.add(new Pair<>(AMEntityRegistry.DEVILS_HOLE_PUPFISH.get(), 1.4F));
+        list.add(new Pair<>(AMEntityRegistry.CATFISH.get(), 1.15F));
+        list.add(new Pair<>(AMEntityRegistry.FLYING_FISH.get(), 1.2F));
+        list.add(new Pair<>(AMEntityRegistry.SKELEWAG.get(), 0.5F));
+        list.add(new Pair<>(AMEntityRegistry.RAIN_FROG.get(), 1.8F));
+        list.add(new Pair<>(AMEntityRegistry.POTOO.get(), 1.2F));
+        list.add(new Pair<>(AMEntityRegistry.MUDSKIPPER.get(), 1.2F));
+        list.add(new Pair<>(AMEntityRegistry.RHINOCEROS.get(), 0.4F));
+        list.add(new Pair<>(AMEntityRegistry.SUGAR_GLIDER.get(), 1.3F));
+        list.add(new Pair<>(AMEntityRegistry.FARSEER.get(), 0.6F));
+        list.add(new Pair<>(AMEntityRegistry.SKREECHER.get(), 0.77F));
+        list.add(new Pair<>(AMEntityRegistry.UNDERMINER.get(), 0.65F));
+        list.add(new Pair<>(AMEntityRegistry.MURMUR.get(), 0.65F));
+    });
+
+    public static List<Pair<EntityType, Float>> getMobIcons() {
+        return MOB_ICONS;
+    }
+
+
+}
