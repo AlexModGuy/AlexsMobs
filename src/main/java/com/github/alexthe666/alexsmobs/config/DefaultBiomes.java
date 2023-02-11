e11156eb094e80a218ca48eaaf7cc762bd9693de
Alexthe668 <alex.rowlands@cox.net>
2023-02-11 10:24:55 -0800

    fix fly spawning in other mod dimensions by default

--- src/main/java/com/github/alexthe666/alexsmobs/config/DefaultBiomes.java
+++ src/main/java/com/github/alexthe666/alexsmobs/config/DefaultBiomes.java
@@ -77,9 +77,7 @@ public class DefaultBiomes {
             .addBiomeEntry(BiomeEntryType.REGISTRY_NAME, false, "terralith:warm_river", 6);
 
     public static final SpawnBiomeData FLY = new SpawnBiomeData()
-            .addBiomeEntry(BiomeEntryType.BIOME_TAG, true, "minecraft:is_end", 0)
-            .addBiomeEntry(BiomeEntryType.BIOME_TAG, true, "minecraft:is_nether", 0)
-            .addBiomeEntry(BiomeEntryType.BIOME_TAG, true, "minecraft:is_ocean", 0);
+            .addBiomeEntry(BiomeEntryType.BIOME_TAG, false, "minecraft:is_overworld", 0);
 
     public static final SpawnBiomeData HUMMINGBIRD = new SpawnBiomeData()
             .addBiomeEntry(BiomeEntryType.REGISTRY_NAME, false, "minecraft:flower_forest", 0)
