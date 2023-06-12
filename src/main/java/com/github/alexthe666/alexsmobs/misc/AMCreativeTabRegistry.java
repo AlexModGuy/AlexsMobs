869ee0238b25112474742f664392c10544ebffe5
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2023-06-12 09:06:57 -0600

    fixed creative tab placement

--- src/main/java/com/github/alexthe666/alexsmobs/misc/AMCreativeTabRegistry.java
+++ src/main/java/com/github/alexthe666/alexsmobs/misc/AMCreativeTabRegistry.java
@@ -6,6 +6,7 @@ import com.github.alexthe666.alexsmobs.item.CustomTabBehavior;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.registries.DeferredRegister;
@@ -18,6 +19,7 @@ public class AMCreativeTabRegistry {
 
     public static final RegistryObject<CreativeModeTab> TAB = DEF_REG.register(AlexsMobs.MODID, () -> CreativeModeTab.builder()
             .title(Component.translatable("itemGroup." + AlexsMobs.MODID))
+            .withTabsBefore(CreativeModeTabs.SPAWN_EGGS)
             .icon(() -> new ItemStack(AMItemRegistry.TAB_ICON.get()))
             .displayItems((enabledFeatures, output) -> {
                 for(RegistryObject<Item> item : AMItemRegistry.DEF_REG.getEntries()){
