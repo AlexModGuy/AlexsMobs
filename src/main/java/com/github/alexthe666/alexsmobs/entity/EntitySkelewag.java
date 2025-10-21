311906495b3380827482fe385411c1179e6e543d
kvnmtz <70778888+kvnmtz@users.noreply.github.com>
2025-10-21 21:59:02 +0200

    fix: correctly restrict skelewags spawned at structures
    The previous if-statement never returned true

--- src/main/java/com/github/alexthe666/alexsmobs/entity/EntitySkelewag.java
+++ src/main/java/com/github/alexthe666/alexsmobs/entity/EntitySkelewag.java
@@ -231,7 +231,7 @@ public class EntitySkelewag extends Monster implements IAnimatedEntity {
             drowned.startRiding(this);
             worldIn.addFreshEntityWithPassengers(drowned);
         }
-        if(reason == MobSpawnType.STRUCTURE){
+        if (AMConfig.restrictSkelewagSpawns) {
             this.restrictTo(this.blockPosition(), 15);
         }
         return super.finalizeSpawn(worldIn, difficultyIn, reason, spawnDataIn, dataTag);
