9c9400cde906d5cb93077baa3b68f24e2d96e350
Alex Lee <islicy@outlook.com>
2026-01-03 11:08:49 +0800

    Refactor message registration to use bidirectional communication for mosquito dismount and entity position sync

--- src/main/java/com/github/alexthe666/alexsmobs/AlexsMobs.java
+++ src/main/java/com/github/alexthe666/alexsmobs/AlexsMobs.java
@@ -140,12 +140,14 @@ public class AlexsMobs {
         // Server to Client messages
         registrar.playToClient(MessageCrowDismount.TYPE, MessageCrowDismount.CODEC, MessageCrowDismount::handle);
         registrar.playToClient(MessageCrowMountPlayer.TYPE, MessageCrowMountPlayer.CODEC, MessageCrowMountPlayer::handle);
-        registrar.playToClient(MessageMosquitoDismount.TYPE, MessageMosquitoDismount.CODEC, MessageMosquitoDismount::handle);
+        // Bidirectional - sent from client (falconry glove launch) and from server (sendMSGToAll for sync)
+        registrar.playBidirectional(MessageMosquitoDismount.TYPE, MessageMosquitoDismount.CODEC, MessageMosquitoDismount::handle);
         registrar.playToClient(MessageMosquitoMountPlayer.TYPE, MessageMosquitoMountPlayer.CODEC, MessageMosquitoMountPlayer::handle);
         registrar.playToClient(MessageKangarooEat.TYPE, MessageKangarooEat.CODEC, MessageKangarooEat::handle);
         registrar.playToClient(MessageKangarooInventorySync.TYPE, MessageKangarooInventorySync.CODEC, MessageKangarooInventorySync::handle);
         registrar.playToClient(MessageStartDancing.TYPE, MessageStartDancing.CODEC, MessageStartDancing::handle);
-        registrar.playToClient(MessageSyncEntityPos.TYPE, MessageSyncEntityPos.CODEC, MessageSyncEntityPos::handle);
+        // Bidirectional - sent from client (falconry glove launch) and from server (sendMSGToAll for sync)
+        registrar.playBidirectional(MessageSyncEntityPos.TYPE, MessageSyncEntityPos.CODEC, MessageSyncEntityPos::handle);
         registrar.playToClient(MessageSendVisualFlagFromServer.TYPE, MessageSendVisualFlagFromServer.CODEC, MessageSendVisualFlagFromServer::handle);
         registrar.playToClient(MessageSetPupfishChunkOnClient.TYPE, MessageSetPupfishChunkOnClient.CODEC, MessageSetPupfishChunkOnClient::handle);
         registrar.playToClient(MessageTarantulaHawkSting.TYPE, MessageTarantulaHawkSting.CODEC, MessageTarantulaHawkSting::handle);
