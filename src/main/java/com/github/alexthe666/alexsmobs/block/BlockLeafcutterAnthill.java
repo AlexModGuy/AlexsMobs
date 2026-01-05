225500e7d0c3a54db995cd1c80857fb245f7975f
Alex <islicy@outlook.com>
2026-01-05 22:11:05 +0900

    Refactor BlockLeafcutterAnthill: update use method to use ItemInteractionResult for improved item handling

--- src/main/java/com/github/alexthe666/alexsmobs/block/BlockLeafcutterAnthill.java
+++ src/main/java/com/github/alexthe666/alexsmobs/block/BlockLeafcutterAnthill.java
@@ -13,6 +13,7 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.item.ItemEntity;
@@ -50,19 +51,19 @@ public class BlockLeafcutterAnthill extends BaseEntityBlock {
         super(BlockBehaviour.Properties.of().sound(SoundType.GRAVEL).strength(0.75F));
     }
 
-    public InteractionResult use(BlockState state, Level worldIn, BlockPos pos, Player player, InteractionHand handIn, BlockHitResult hit) {
+    @Override
+    public ItemInteractionResult useItemOn(ItemStack heldItem, BlockState state, Level worldIn, BlockPos pos, Player player, InteractionHand handIn, BlockHitResult hit) {
         if (worldIn.getBlockEntity(pos) instanceof TileEntityLeafcutterAnthill) {
             TileEntityLeafcutterAnthill hill = (TileEntityLeafcutterAnthill) worldIn.getBlockEntity(pos);
-            ItemStack heldItem = player.getItemInHand(handIn);
             if (heldItem.getItem() == AMItemRegistry.GONGYLIDIA.get() && hill.hasQueen()) {
                 hill.releaseQueens();
                 if (!player.isCreative()) {
                     heldItem.shrink(1);
                 }
+                return ItemInteractionResult.SUCCESS;
             }
-            return InteractionResult.SUCCESS;
         }
-        return InteractionResult.PASS;
+        return super.useItemOn(heldItem, state, worldIn, pos, player, handIn, hit);
     }
 
 
