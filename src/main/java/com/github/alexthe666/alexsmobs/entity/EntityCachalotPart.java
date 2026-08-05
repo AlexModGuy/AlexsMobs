cf8acb6ab33e0a523cbb810602161c9bb985e58b
digi2303 <132196028+digi2303@users.noreply.github.com>
2026-08-05 23:19:14 +0300

    Restore the spawn packet guard on multipart entities

--- src/main/java/com/github/alexthe666/alexsmobs/entity/EntityCachalotPart.java
+++ src/main/java/com/github/alexthe666/alexsmobs/entity/EntityCachalotPart.java
@@ -9,6 +9,7 @@ import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerEntity;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
@@ -83,7 +84,7 @@ public class EntityCachalotPart extends PartEntity<EntityCachalotWhale> {
         return this == entityIn || this.getParent() == entityIn;
     }
 
-    public Packet<ClientGamePacketListener> getAddEntityPacket() {
+    public Packet<ClientGamePacketListener> getAddEntityPacket(ServerEntity entity) {
         throw new UnsupportedOperationException();
     }
 
