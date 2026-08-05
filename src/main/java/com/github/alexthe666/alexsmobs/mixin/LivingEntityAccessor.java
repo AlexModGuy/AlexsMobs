75dcca13c3b1b3058e746603b788ab2acbe36216
digi2303 <132196028+digi2303@users.noreply.github.com>
2026-08-05 17:37:16 +0300

    Restore jump detection for the Flying Fish Boots boost

---
+++ src/main/java/com/github/alexthe666/alexsmobs/mixin/LivingEntityAccessor.java
@@ -0,0 +1,11 @@
+package com.github.alexthe666.alexsmobs.mixin;
+
+import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(LivingEntity.class)
+public interface LivingEntityAccessor {
+    @Accessor("jumping")
+    boolean getJumping();
+}
